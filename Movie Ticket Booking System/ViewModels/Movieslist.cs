a4606faecc00945bb9d4480fcc7e3d2ce613267c
MarinaFawzy <89097013+MarinaFawzy@users.noreply.github.com>
2023-01-29 05:37:37 -0700

    Movie Ticket Booking project

---
+++ Movie Ticket Booking System/ViewModels/Movieslist.cs
@@ -0,0 +1,14 @@
+﻿using Movie_Ticket_Booking_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Movie_Ticket_Booking_System.ViewModels
+{
+    public class Movieslist
+    {
+        public string CinemaName { get; set; }
+        public List<MovieDetails> movieDetails { get; set; }
+    }
+}
